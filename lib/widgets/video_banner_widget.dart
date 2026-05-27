15d26a41c9db2b298ff626fb92e53376a9cf4a65
2FIT MALL Deploy <chw243527@gmail.com>
2026-05-27 01:22:04 +0000

    fix: 워싱팁 번호 제거 + 상세페이지 배경 흰색

--- lib/widgets/video_banner_widget.dart
+++ lib/widgets/video_banner_widget.dart
@@ -1,4 +1,7 @@
 // video_banner_widget.dart
-// 플랫폼에 따라 Web impl / Stub을 자동 선택하는 진입점
+// kIsWeb 런타임 분기로 웹/네이티브 구현 선택
+// - 웹: HtmlElementView + <video> 태그 (dart:html)
+// - 네이티브: video_player 패키지
+
 export 'video_banner_stub.dart'
     if (dart.library.html) 'video_banner_web_impl.dart';
