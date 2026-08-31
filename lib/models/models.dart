4f195191be361006012c6ee9d54d332a11ad3ebb
chw243527-ux <2.65145463e+08+chw243527-ux@users.noreply.github.com>
2026-08-31 23:11:39 +0000

    fix: import constants for revision policy

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../utils/app_localizations.dart';
+import '../utils/constants.dart';
 
 class ProductModel {
   final String id;
